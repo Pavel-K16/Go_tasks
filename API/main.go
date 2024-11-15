c257bc9dc99fb1aec7903d1a8fa526f1e5bf7e84
Pavel <pklabukov863@gmail.com>
2024-11-15 23:18:33 +0300

    Today's final commit

--- API/main.go
+++ API/main.go
@@ -15,22 +15,26 @@ import (
 // curl -X DELETE http://localhost:8080/item/20
 // http://localhost:8080/item/1
 
-//curl -X POST -H "Content-Type: application/json" -d '{"Name":"John Doe","Description":"john@example.com"}' http://localhost:8080/category
-//curl -X PUT -H "Content-Type: application/json" -d '{"Name":"Дорогие телефоны", "description":"В этой категории телефоны дороже 100к"}' http://localhost:8080/category/2
-//curl -X DELETE http://localhost:8080/category/2
+// curl -X POST -H "Content-Type: application/json" -d '{"Name":"John Doe","Description":"john@example.com"}' http://localhost:8080/category
+// curl -X PUT -H "Content-Type: application/json" -d '{"Name":"Дорогие телефоны", "description":"В этой категории телефоны дороже 100к"}' http://localhost:8080/category/2
+// curl -X DELETE http://localhost:8080/category/2
 func main() {
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/item/{id}", h.GetInfoHandler).Methods("GET")
-	r.HandleFunc("/item", h.CreateProductHandler).Methods("POST")
-	r.HandleFunc("/category", h.CreateCategoryHandler).Methods("POST")
-	r.HandleFunc("/item/{id}", h.UpdateProductHandler).Methods("PUT")
-	r.HandleFunc("/category/{id}", h.UpdateCategoryHandler).Methods("PUT")
-	r.HandleFunc("/item/{id}", h.DeleteProductHandler).Methods("DELETE")
-	r.HandleFunc("/category/{id}", h.DeleteCategoryHandler).Methods("DELETE")
 	r.HandleFunc("/item", h.ShowAllProductsHandler).Methods("GET")
 	r.HandleFunc("/category", h.ShowAllCategoryHandler).Methods("GET")
+
+	r.HandleFunc("/item", h.CreateProductHandler).Methods("POST")
+	r.HandleFunc("/category", h.CreateCategoryHandler).Methods("POST")
+
+	r.HandleFunc("/item/{id}", h.UpdateProductHandler).Methods("PUT")
+	r.HandleFunc("/category/{id}", h.UpdateCategoryHandler).Methods("PUT")
+
+	r.HandleFunc("/item/{id}", h.DeleteProductHandler).Methods("DELETE")
+	r.HandleFunc("/category/{id}", h.DeleteCategoryHandler).Methods("DELETE")
+
 	err := http.ListenAndServe(e.Serv, r)
 	if err != nil {
 		log.Fatal("Ошибка при запуске сервера", err)
